d5146eac16bed4cd7c1637fee0572271cff2307c
AhmadRaza <ahmadraza.azt@gmail.com>
2022-10-16 14:36:10 +0500

    code push

--- app.js
+++ app.js
@@ -2,31 +2,101 @@
 // Developed by Ahmed Raza
 
 const questions = [{
-	'que' : 'Which of the following is a markup language ?',
-	'a' : 'HTML',
-	'b' : 'CSS',
-	'c' : 'JavaScript',
-	'd' : 'PHP',
-	'correct' : 'a'
+	'que' : "What is the total area of Punjab Province?",
+	'a' : "74,521 Km",
+	'b' : "347,190 Sq Km",
+	'c' : "205,344 Sq Km",
+	'd' : "208,354 Sq Km",
+	'correct' : 'c'
 },
 {
-	'que' : 'What was the JavaScript launched ?',
-	'a' : '1996',
-	'b' : '1995',
-	'c' : '1994',
+	'que' : 'Defence day is observed on ___________ ?',
+	'a' : '14 Feb',
+	'b' : '14 August',
+	'c' : '6 September',
+	'd' : 'None of the above',
+	'correct' : 'c'
+},
+{
+	'que' : "Who is the current Test Captain of Pakistani Team",
+	'a' : "Asif Ali",
+	'b' : 'M. Rizwan',
+	'c' : 'Baber Azam',
+	'd' : 'Hassan Ali',
+	'correct' : 'c'
+},
+{
+	'que' : "National code of Pakistan is ?",
+	'a' : "Pak",
+	'b' : 'PK',
+	'c' : 'Pti',
+	'd' : 'None of Them',
+	'correct' : 'b'
+},
+{
+	'que' : 'Who is the current Prime Minister of Pakistan? ',
+	'a' : 'Imran Khan',
+	'b' : 'Shahbaz Shareef',
+	'c' : 'Altaf Hussain',
+	'd' : 'Asif ALi Zardari',
+	'correct' : 'b'
+},
+{
+	'que' : 'How many provinces Pakistan have ?',
+	'a' : '6',
+	'b' : '5',
+	'c' : '4',
 	'd' : 'None of the above',
 	'correct' : 'b'
 },
 {
-	'que' : 'What dose css stands for ?',
-	'a' : 'Hyper text markup language',
-	'b' : 'Cascading style sheet',
-	'c' : 'Json object nation',
-	'd' : 'Halicopters Terminal Motor boats Lamboghinis',
-	'correct' : 'b'
+	'que' : 'Who was the founder of Daily English Newspaper Dawn ?',
+	'a' : 'Liaquat Ali Khan',
+	'b' : 'Quid-e-Azam',
+	'c' : 'Dr. Allama Iqbal',
+	'd' : 'None of them',
+	'correct' : 'a'
+},
+{
+	'que' : 'The 1962 Constitution of Pakistan was passed during the regime of?"',
+	'a' : 'Ayoub Khan',
+	'b' : 'Sikandar Mirza',
+	'c' : 'Ahmed Raza',
+	'd' : 'None of the above',
+	'correct' : 'a'
+},
+{
+	'que' : 'Who was the hero of Pak India 1965 war ?',
+	'a' : 'Waqa Zaka',
+	'b' : 'Tahira Bhatti',
+	'c' : 'Major Raja Aziz Bhatti',
+	'd' : 'Raja Muham Serwar',
+	'correct' : 'c'
 }
 ];
 // console.log('Developed by AhmedRaza');
+
+//time function
+(() => {
+	const dateTimeDiv = document.querySelector('#dateDiv');
+	const dateDiv = document.createElement('div')
+	const time = document.createElement('div')
+	let tim = moment().format("h:mm a");
+	setInterval(() => {
+	        tim = moment().format("h:mm:ss a");
+	}, 1000);
+	// console.log(tim)
+	dateDiv.appendChild(document.createTextNode(`${moment().format("D MMM YYYY")}`))
+	time.appendChild(document.createTextNode(`${tim}`))
+	dateTimeDiv.appendChild(dateDiv)
+	dateTimeDiv.appendChild(time)
+	// console.log(TODAY)
+})();
+
+
+
+
+
 
 let index = 0;
 let total = questions.length;
@@ -140,3 +210,8 @@ function darkMode() {
 // dark mode end
 
 loadQuestion();
+
+
+function resetData  () {
+	location.reload();
+}
